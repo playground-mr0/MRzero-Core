002b0213f3b9b4e5b87623a5440e312fc38d09ef
playground-mr0 <playgroundmr0@gmail.com>
2024-11-25 13:25:49 +0100

    Add files via upload

---
+++ documentation/CI_CD_results/Comparison_results_workflow (1).py
@@ -0,0 +1,13 @@
+import numpy as np
+
+vectorGroundTruth = np.load("./CI_CD_results/signals_GroundTruth/FID_signal.npz")
+vectorLastPush = np.load("./CI_CD_results/signals_CICD_last_run/FID_signal.npz")
+
+vectorGroundTruth = vectorGroundTruth['signal'].astype(np.complex64)
+vectorLastPush = vectorLastPush['signal'].astype(np.complex64)
+
+class TestPythonScript:
+
+    def test_compare_PlaygroundMR0_results(self):
+        assert np.array_equal(vectorGroundTruth, vectorLastPush)
+
