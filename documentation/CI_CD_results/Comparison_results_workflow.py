2ef18d0ee0591f1d0a4826d8be92ceea7c84913e
playground-mr0 <playgroundmr0@gmail.com>
2024-11-25 15:21:44 +0100

    Update Comparison_results_workflow.py

--- documentation/CI_CD_results/Comparison_results_workflow.py
+++ documentation/CI_CD_results/Comparison_results_workflow.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-vectorGroundTruth = np.load("./CI_CD_results/signals_GroundTruth/FID_signal.npz")
-vectorLastPush = np.load("./CI_CD_results/signals_CICD_last_run/FID_signal.npz")
+vectorGroundTruth = np.load("./documentation/CI_CD_results/signals_GroundTruth/FID_signal.npz")
+vectorLastPush = np.load("./documentation/CI_CD_results/signals_CICD_last_run/FID_signal.npz")
 
 vectorGroundTruth = vectorGroundTruth['signal'].astype(np.complex64)
 vectorLastPush = vectorLastPush['signal'].astype(np.complex64)
