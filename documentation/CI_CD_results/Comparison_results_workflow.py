88027586e0a3727b688842a17987cbcb40fea66a
playground-mr0 <playgroundmr0@gmail.com>
2024-11-25 15:29:41 +0100

    Update Comparison_results_workflow.py

--- documentation/CI_CD_results/Comparison_results_workflow.py
+++ documentation/CI_CD_results/Comparison_results_workflow.py
@@ -1,5 +1,22 @@
 import numpy as np
+import os
 
+#1. we go through each npz file in ./documentation/CI_CD_results/signals_GroundTruth
+#2. we search for a file with identical name in folder ./documentation/CI_CD_results/signals_CICD_last_run
+#3. we load the numpy arrays from these 2 files
+#4. Compute difference and save logical value = difference
+#5. For each diff we echo a message saying for which signal and if it passed or not
+#6. The test final value will be the && operation between all the difs
+
+folder_Ground_Truth = "./documentation/CI_CD_results/signals_GroundTruth"
+folder_Last_Push = "./documentation/CI_CD_results/signals_CICD_last_run"
+
+for filename in os.listdir(folder_Ground_Truth):
+    if filename.endswith('.npz'):
+        file_path = os.path.join(directory, filename)
+        print(f'Processing file: {file_path}')
+
+'''
 vectorGroundTruth = np.load("./documentation/CI_CD_results/signals_GroundTruth/FID_signal.npz")
 vectorLastPush = np.load("./documentation/CI_CD_results/signals_CICD_last_run/FID_signal.npz")
 
@@ -10,4 +27,4 @@ class TestPythonScript:
 
     def test_compare_PlaygroundMR0_results(self):
         assert np.array_equal(vectorGroundTruth, vectorLastPush)
-
+'''
